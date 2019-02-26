66e4278ff1ba9f5e44b0fe74d70b770423f1f537
MaciejKut <maciej.kutzmann@gmail.com>
2019-02-26 23:13:04 +0100

    Add exercise with read file server

---
+++ index.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var fs = require('fs');
+
+
+fs.readFile('./index.html', 'utf-8', function (err, data) {
+
+    if (err) throw error;
+    var server = http.createServer();
+
+    server.on('request', function (request, response) {
+        response.setHeader('Content-Type', 'text/html');
+        response.statusCode = 200;
+        if (request.method === 'GET' && request.url == "/") {
+            response.write(data);
+            response.end();
+        } else {
+            response.statusCode = 404;
+            fs.readFile('./cat.jpg', function (err, data) {
+                response.writeHead(404, {
+                    'Content-Type': 'image/jpeg'
+                });
+                response.end(data);
+            });
+
+        }
+    });
+    server.listen(9100);
+    console.log('Server running.');
+});
