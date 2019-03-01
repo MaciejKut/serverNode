039595249e7525c466d992a770c10040f00d0faf
MaciejKut <maciej.kutzmann@gmail.com>
2019-03-01 14:56:17 +0100

    Update logic of index.js

--- index.js
+++ index.js
@@ -2,28 +2,30 @@ var http = require('http');
 var fs = require('fs');
 
 
-fs.readFile('./index.html', 'utf-8', function (err, data) {
 
-    if (err) throw error;
-    var server = http.createServer();
+var server = http.createServer();
 
-    server.on('request', function (request, response) {
-        response.setHeader('Content-Type', 'text/html');
-        response.statusCode = 200;
-        if (request.method === 'GET' && request.url == "/") {
+server.on('request', function (request, response) {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+    if (request.method === 'GET' && request.url == "/") {
+        fs.readFile('./index.html', 'utf-8', function (err, data) {
+
+            if (err) throw error;
             response.write(data);
             response.end();
-        } else {
-            response.statusCode = 404;
-            fs.readFile('./cat.jpg', function (err, data) {
-                response.writeHead(404, {
-                    'Content-Type': 'image/jpeg'
-                });
-                response.end(data);
+        });
+    } else {
+        response.statusCode = 404;
+        fs.readFile('./cat.jpg', function (err, data) {
+            response.writeHead(404, {
+                'Content-Type': 'image/jpeg'
             });
+            response.end(data);
+        });
 
-        }
-    });
-    server.listen(9100);
-    console.log('Server running.');
+    }
+
 });
+server.listen(9100);
+console.log('Server running.');
